3c4ee54795a54c78d68144435f24f37a99a496e3
kyra <kyra@10.133.12.71>
2023-01-18 11:25:51 -0500

    Initial Project Commit

---
+++ CPS2232/src/lab1/Program2.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		String s = "spring2023";
+		for (int i=0; i < s.length(); i++) {
+			System.out.println(s.charAt(i));
+		}
+	}
+
+}
